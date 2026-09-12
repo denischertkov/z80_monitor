81c0a6cb0b18efc6a51848d8845496bab4d3ce75
Denis Chertkov <denis@chertkov.info>
2026-09-12 17:12:11 +0200

    Port monitor added (#3)
    * Port tester

--- rom_loader.asm
+++ rom_loader.asm
@@ -3,6 +3,7 @@
 ; This program disable the ROM on 0000-3FFF and copy to this place the image from 5000-8FFF
 ; At the end it jumps to 0000 to start the new monitor from RAM
 ;
+; I used this program to test the new monitor image in RAM without burning it to ROM.
 ; Denis Chertkov, denis@chertkov.info ,20260911
 
         ORG $4100
@@ -11,7 +12,6 @@ start:
         DI
 
         ; Disable ROM: write something to the port 38h
-        ; XOR A
         OUT ($38),A
 
         ; New monitor image:
@@ -22,6 +22,8 @@ start:
         LDIR
 
         ; Write 55h to 000Dh (unused 3 bytes) to test RAM
+        ; You can use the M command in the new Monitor to see this value in RAM 
+        ; instead of the ROM string from 000Dh
         LD HL,000Dh
         LD (HL),'R'
         INC HL
